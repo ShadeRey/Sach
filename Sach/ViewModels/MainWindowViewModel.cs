679a074d21e361722d805eb229cc32b3e10f83fb
ShadeRey <andryusha_shadrin@bk.ru>
2024-01-13 11:00:14 +0700

    feat: update gui

--- Sach/ViewModels/MainWindowViewModel.cs
+++ Sach/ViewModels/MainWindowViewModel.cs
@@ -57,4 +57,12 @@ public class MainWindowViewModel : ViewModelBase
         get => _playersList;
         set => this.RaiseAndSetIfChanged(ref _playersList, value);
     }
+    
+    private string _fish =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    public string Fish
+    {
+        get => _fish;
+    }
 }
